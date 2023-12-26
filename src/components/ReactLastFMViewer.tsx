8c9aa9d1cc57ba73944419d0baa21d8712f7ad64
ZOASR <44146518+ZOASR@users.noreply.github.com>
2023-12-26 19:04:41 +0200

    performance: added preconnect links to root component

--- src/components/ReactLastFMViewer.tsx
+++ src/components/ReactLastFMViewer.tsx
@@ -50,98 +50,108 @@ const ReactLastFMViewer = ({ api_key, user, updateInterval }: Props) => {
 	});
 
 	return (
-		<lfmContext.Provider value={{ colors: colors, track: track }}>
-			<div
-				className={
-					styles.lfmvCard +
-					" glass relative mx-auto flex h-full w-full flex-col rounded-lg p-4 shadow-xl ring-2 ring-slate-950/5"
-				}
-				style={{ background: colors?.primary }}
-			>
-				{track instanceof Error ? (
-					<ErrorView message={message} />
-				) : (
-					<>
-						<figure
-							className="mx-auto mb-2 h-auto overflow-hidden rounded-lg border-inherit"
-							style={{
-								boxShadow: `0 0 20px ${colors?.secondary}99`
-							}}
-						>
-							{track?.lastfmImages &&
-							track?.lastfmImages[3]["#text"] ? (
-								<img
-									className="block h-full w-full overflow-hidden object-cover align-middle"
-									src={track.lastfmImages[3]["#text"]}
-									alt="Album Cover"
-								/>
-							) : track?.MBImages ? (
-								<img
-									className="block h-full w-full overflow-hidden object-cover align-middle"
-									src={track.MBImages[0].image}
-									alt="Album Cover"
-								/>
-							) : (
-								<img
-									src={disc}
-									className=""
-									alt="Default album cover thumbnail"
-								/>
-							)}
-						</figure>
-
-						<div className="flex h-min flex-col gap-1 drop-shadow-lg filter">
-							{track?.nowplaying ? <TrackProgressBar /> : ""}
-							<h1
-								className="shadow:lg mx-auto mt-1 text-center text-xs font-bold sm:text-base"
-								style={{ color: colors?.secondary }}
+		<>
+			{/* preconnects */}
+			<link href="https://lastfm.freetls.fastly.net" rel="preconnect" />
+			<link href="https://archive.org" rel="preconnect" />
+			<link href="https://coverartarchive.org" rel="preconnect" />
+			<link href="http://coverartarchive.org" rel="preconnect" />
+			<link href="https://musicbrainz.org" rel="preconnect" />
+			<link href="http://ws.audioscrobbler.com" rel="preconnect" />
+			{/* preconnects */}
+			<lfmContext.Provider value={{ colors: colors, track: track }}>
+				<div
+					className={
+						styles.lfmvCard +
+						" glass relative mx-auto flex h-full w-full flex-col rounded-lg p-4 shadow-xl ring-2 ring-slate-950/5"
+					}
+					style={{ background: colors?.primary }}
+				>
+					{track instanceof Error ? (
+						<ErrorView message={message} />
+					) : (
+						<>
+							<figure
+								className="mx-auto mb-2 h-auto overflow-hidden rounded-lg border-inherit"
+								style={{
+									boxShadow: `0 0 20px ${colors?.secondary}99`
+								}}
 							>
-								{loading ? (
-									<div className="skeleton h-8 w-fit"></div>
-								) : track?.trackName ? (
-									track?.trackName
+								{track?.lastfmImages &&
+								track?.lastfmImages[3]["#text"] ? (
+									<img
+										className="block h-full w-full overflow-hidden object-cover align-middle"
+										src={track.lastfmImages[3]["#text"]}
+										alt="Album Cover"
+									/>
+								) : track?.MBImages ? (
+									<img
+										className="block h-full w-full overflow-hidden object-cover align-middle"
+										src={track.MBImages[0].image}
+										alt="Album Cover"
+									/>
 								) : (
-									"Track title not available"
+									<img
+										src={disc}
+										className=""
+										alt="Default album cover thumbnail"
+									/>
 								)}
-							</h1>
-							<div
-								style={{ color: colors?.secondary }}
-								className="flex flex-col gap-2 text-xs"
-							>
-								<LoadingSkeleton
-									user={user}
-									api_key={api_key}
-									updateInterval={updateInterval}
-									fallbackMsg="Artist name not available"
+							</figure>
+
+							<div className="flex h-min flex-col gap-1 drop-shadow-lg filter">
+								{track?.nowplaying ? <TrackProgressBar /> : ""}
+								<h1
+									className="shadow:lg mx-auto mt-1 text-center text-xs font-bold sm:text-base"
+									style={{ color: colors?.secondary }}
 								>
-									{track?.artistName && (
-										<span className="flex items-center justify-center gap-1">
-											<FaRegUser />
-											{track?.artistName}
-										</span>
+									{loading ? (
+										<div className="skeleton h-8 w-fit"></div>
+									) : track?.trackName ? (
+										track?.trackName
+									) : (
+										"Track title not available"
 									)}
-								</LoadingSkeleton>
-								<LoadingSkeleton
-									user={user}
-									api_key={api_key}
-									updateInterval={updateInterval}
-									fallbackMsg="Album name not available"
+								</h1>
+								<div
+									style={{ color: colors?.secondary }}
+									className="flex flex-col gap-2 text-xs"
 								>
-									{track?.albumTitle && (
-										<span className="flex items-center justify-center gap-1">
-											<FaCompactDisc />
-											{track?.albumTitle}
-										</span>
-									)}
-								</LoadingSkeleton>
+									<LoadingSkeleton
+										user={user}
+										api_key={api_key}
+										updateInterval={updateInterval}
+										fallbackMsg="Artist name not available"
+									>
+										{track?.artistName && (
+											<span className="flex items-center justify-center gap-1">
+												<FaRegUser />
+												{track?.artistName}
+											</span>
+										)}
+									</LoadingSkeleton>
+									<LoadingSkeleton
+										user={user}
+										api_key={api_key}
+										updateInterval={updateInterval}
+										fallbackMsg="Album name not available"
+									>
+										{track?.albumTitle && (
+											<span className="flex items-center justify-center gap-1">
+												<FaCompactDisc />
+												{track?.albumTitle}
+											</span>
+										)}
+									</LoadingSkeleton>
+								</div>
+								<PastTracks />
+								<CardFooter user={user} colors={colors} />
 							</div>
-							<PastTracks />
-							<CardFooter user={user} colors={colors} />
-						</div>
-					</>
-				)}
-			</div>
-		</lfmContext.Provider>
+						</>
+					)}
+				</div>
+			</lfmContext.Provider>
+		</>
 	);
 };
 
