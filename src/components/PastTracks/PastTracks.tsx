7b5752dc02a9c6d092c73f4a10b681e2e42d081a
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-01-04 17:31:17 +0200

    refactor: used a simpler array method to display past tracks

--- src/components/PastTracks/PastTracks.tsx
+++ src/components/PastTracks/PastTracks.tsx
@@ -4,8 +4,6 @@ import { FaRegUser, FaCalendar } from "react-icons/fa";
 import styles from "@lastfm-viewer/ui/PastTracks.module.css";
 import LoadingSkeleton from "../LoadingSkeleton/LoadingSkeleton";
 
-import { cloneArray } from "@lastfm-viewer/utils/utils";
-
 const PastTracks = () => {
 	const context = useContext(lfmContext);
 	return (
@@ -30,70 +28,59 @@ const PastTracks = () => {
 					{context.track instanceof Error
 						? ""
 						: context.track?.pastTracks
-						  ? cloneArray(context.track?.pastTracks)
-									.splice(1, context.track?.pastTracks.length)
-									.map((track_) => {
-										return (
-											<div
-												key={
-													track_.date["#text"] +
-													track_.name
-												}
-												className={
-													styles.pastTracks__track
-												}
-											>
-												<div className="divider m-0.5 h-min"></div>
-												<div
+								?.filter((_, idx) => idx !== 0)
+								.map((track_) => {
+									return (
+										<div
+											key={
+												track_.date["#text"] +
+												track_.name
+											}
+											className={styles.pastTracks__track}
+										>
+											<div className="divider m-0.5 h-min"></div>
+											<div className={styles.scrollable}>
+												<a
+													href={track_.url}
+													target="_blank"
 													className={
-														styles.scrollable
+														styles.pastTracks__trackTitle
 													}
+													style={{
+														color: context.colors
+															?.secondary
+													}}
 												>
-													<a
-														href={track_.url}
-														target="_blank"
-														className={
-															styles.pastTracks__trackTitle
-														}
-														style={{
-															color: context
-																.colors
-																?.secondary
-														}}
-													>
-														{track_.name}
-													</a>
-													<span
-														className={
-															styles.scrollable__artist
-														}
-														style={{
-															color: context
-																.colors
-																?.secondary
-														}}
-													>
-														<FaRegUser />
-														{track_.artist["#text"]}
-													</span>
-													<span
-														className={
-															styles.scrollable__date
-														}
-														style={{
-															color: context
-																.colors
-																?.secondary
-														}}
-													>
-														<FaCalendar />
-														{track_.date["#text"]}
-													</span>
-												</div>
+													{track_.name}
+												</a>
+												<span
+													className={
+														styles.scrollable__artist
+													}
+													style={{
+														color: context.colors
+															?.secondary
+													}}
+												>
+													<FaRegUser />
+													{track_.artist["#text"]}
+												</span>
+												<span
+													className={
+														styles.scrollable__date
+													}
+													style={{
+														color: context.colors
+															?.secondary
+													}}
+												>
+													<FaCalendar />
+													{track_.date["#text"]}
+												</span>
 											</div>
-										);
-									})
-						  : ""}
+										</div>
+									);
+								})}
 				</LoadingSkeleton>
 			</div>
 		</>
