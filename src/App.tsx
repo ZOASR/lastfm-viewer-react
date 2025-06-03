0a77f298a693fa7f0be56a9c4057ed070b19241d
ZOASR <44146518+ZOASR@users.noreply.github.com>
2025-06-03 13:43:50 +0300

    dev: added a button to toggle between dev component and live component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import Test from "./Test";
 import ReactLastFMViewer from "./components/ReactLastFMViewer";
 
 function App() {
+	const [isTest, setIsTest] = useState(false);
 	return (
 		<>
-			<ReactLastFMViewer
-				user="ZOASR"
-				api_key={import.meta.env.VITE_API_KEY}
-				updateInterval={20000}
-			/>
-			<Test api_key="very_wrong_api_key" />
+			<button onClick={() => setIsTest(!isTest)}>
+				View {isTest ? "Live" : "Test"}
+			</button>
+			{!isTest ? (
+				<ReactLastFMViewer
+					user="ZOASR"
+					api_key={import.meta.env.VITE_API_KEY}
+					updateInterval={20000}
+				/>
+			) : (
+				<Test api_key={import.meta.env.VITE_API_KEY} />
+			)}
 		</>
 	);
 }
